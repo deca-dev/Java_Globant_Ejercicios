87ca17c4346a99a433a95b5976c77c55057566a6
David Castro <davidernesto.castroanaya@gmail.com>
2023-07-04 18:58:31 -0600

    First Commit

---
+++ src/introjava_ejercicio01/IntroJava_ejercicio01.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introjava_ejercicio01;
+
+/**
+ *
+ * @author CastroD2
+ */
+public class IntroJava_ejercicio01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre;
+        int edad;
+        double altura;
+        boolean soltero;
+        String direccion;
+        int numero;
+    }
+    
+}
